f1148f3f40582a255fe8239ef4acbb6a61c97f44
anamitrajoshi <anamitra.joshi@gmail.com>
2023-11-20 23:38:02 +0530

    initial backend for change_menu added

---
+++ Frontend/change_menu.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request, redirect
+import firebase_admin
+from firebase_admin import credentials
+
+app = Flask(__name__)
+
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate("C:\\Users\\Dell\\Downloads\\food-with-love-de5c4-firebase-adminsdk-udmlc-2ddd1fd6a7.json")
+firebase_admin.initialize_app(cred, {
+    "databaseURL": "https://food-with-love-de5c4-default-rtdb.firebaseio.com/"
+})
+
+# Initialize Realtime Database
+ref = firebase_admin.reference()
+
+@app.route('/')
+def index():
+    return render_template('change_menu.html')
+
+@app.route('/update-profile', methods=['POST'])
+def update_profile():
+    if request.method == 'POST':
+        # Extract form data
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        # Add more fields as needed
+
+        # Update Realtime Database with the user data
+        users_ref = ref.child('users').child(username)
+        users_ref.set({
+            'username': username,
+            'email': email,
+            'password': password,
+            # Add more fields as needed
+        })
+
+    return redirect('/')
+
+@app.route('/update-menu', methods=['POST'])
+def update_menu():
+    if request.method == 'POST':
+        # Extract form data for the menu update
+        # You need to add more lines to extract all the dish and price values
+
+        # Example:
+        dish1 = request.form.get('dish1')
+        price1 = request.form.get('price1')
+        dish2 = request.form.get('dish2')
+        price2 = request.form.get('price2')
+        dish3 = request.form.get('dish3')
+        price3 = request.form.get('price3')
+        dish4 = request.form.get('dish4')
+        price4 = request.form.get('price4')
+        dish5 = request.form.get('dish5')
+        price5 = request.form.get('price5')
+
+        # Repeat for dish2, price2, ..., dish5, price5
+
+        # Update Realtime Database with the menu data
+        # This is just a basic example, you might want to structure your data differently
+        menu_ref = ref.child('menu')
+        menu_ref.set({
+            'dish1': dish1,
+            'price1': price1,
+            # Add more fields as needed
+        })
+
+    return redirect('/kitchens_page')
+
+if __name__ == '__main__':
+    app.run(debug=True)
